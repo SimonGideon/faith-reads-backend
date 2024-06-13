94824d9ef7ece0f95e42166ddf5a5a24f6a5eaca
SimonGideon <simongideon918@gmail.com>
2024-06-13 13:55:08 +0300

    handle responds

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer
   include JSONAPI::Serializer
   attributes :id, :email, :first_name, :last_name, :phone, :home_church, :residence, :city, :date_of_birth, :membership_number, :created_at
+  attribute :created_date do |user|
+    user.created_at && user.created_at.strftime('%m/%d/%Y')
+  end
 end
