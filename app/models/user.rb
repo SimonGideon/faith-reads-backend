4d2ac525943db3f14e36685e6f10987ad5c0bf50
SimonGideon <simongideon918@gmail.com>
2024-06-14 11:19:54 +0300

    initialize current user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   before_create :generate_jti
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,6 +8,10 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   private
+
+  def jwt_revoked?(payload, user)
+    !user.present? || user.jti != payload["jti"]
+  end
 
   def generate_jti
     self.jti ||= SecureRandom.uuid
